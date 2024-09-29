6ba52bd99f1e12104bd8a5685b3508a6bb0f4d38
Benjamin Edlinger <bedlinger@student.tgm.ac.at>
2024-09-29 12:18:06 +0200

    WortEintrag hinzufügen ermöglichen

--- src/main/java/at/ac/tgm/bedlinger/controller/Controller.java
+++ src/main/java/at/ac/tgm/bedlinger/controller/Controller.java
@@ -11,7 +11,7 @@ import javax.swing.*;
  * Repräsentiert den Controller des Programms
  *
  * @author Benjamin Edlinger
- * @version 2024-09-28
+ * @version 2024-09-29
  */
 public class Controller {
     private WortTrainer wortTrainer;
@@ -56,10 +56,23 @@ public class Controller {
         boolean result = wortTrainer.check(wort);
         view.setCounter(wortTrainer.getCounterAbgefragt(), wortTrainer.getCounterKorrekt(), wortTrainer.getCounterFalsch());
         view.setVorherigerVersuch(wortTrainer.getVorherigerVersuchKorrekt());
-        if (result) {
+        if (result)
             view.setBild(wortTrainer.getAktuellerWortEintrag().getUrl());
-        } else {
-            view.setEingabe("");
+        view.setEingabe("");
+    }
+
+    /**
+     * Fügt einen neuen WortEintrag hinzu, aber nur, wenn das Wort und die URL korrekt sind.
+     * Wenn ein Fehler auftritt, wird eine Fehlermeldung angezeigt.
+     *
+     * @param wort das hinzuzufügende Wort
+     * @param url  die URL des hinzuzufügenden Wortes
+     */
+    public void addWortEintrag(String wort, String url) {
+        try {
+            wortTrainer.addWortEintrag(wort, url);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Fehler beim Hinzufügen: " + e.getMessage(), "Fehler", JOptionPane.ERROR_MESSAGE);
         }
     }
 
